8c3d76279df06542d6e05034b1f2f01dfcc1b2c3
Mathias <Mathias@LAPTOP-9FSN3NE0>
2019-09-30 13:47:09 +0200

    Add project files.

---
+++ MinAgent/MinAgentFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AIFramework;
+using AIFramework.Entities;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace MinAgent
+{
+    public class MinAgentFactory : AgentFactory
+    {
+        public override Agent CreateAgent(IPropertyStorage propertyStorage)
+        {
+            return new MinAgent(propertyStorage);
+        }
+
+        public override Agent CreateAgent(Agent parent1, Agent parent2, IPropertyStorage propertyStorage)
+        {
+            return new MinAgent(propertyStorage);
+        }
+
+        public override Type ProvidedAgentType
+        {
+            get { return typeof(MinAgent); }
+        }
+
+        public override string Creators
+        {
+            get { return "Gucci gang"; }
+        }
+
+        public override void RegisterWinners(List<Agent> sortedAfterDeathTime)
+        {
+            //Do data collection - Perhaps used to evolutionary algoritmen
+        }
+    }
+}
