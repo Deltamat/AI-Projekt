254c62501f2f4aa6c0fc1a643ffb6ab616d316cf
Overlord9001 <45371858+Overlord9001@users.noreply.github.com>
2019-10-08 14:51:51 +0200

    ryddet lidt op i rækkefølgen i minagent og ændret navn på en variabel i stateattack

--- MinAgent/States/StateAttack.cs
+++ MinAgent/States/StateAttack.cs
@@ -14,10 +14,11 @@ namespace MinAgent
         {
             if (agent.closeEnemyAgents.Count > 0 && agent.Strength > 0)
             {
-                if (AIVector.Distance(agent.Position, agent.closeEnemyAgents[0].Position) > AIModifiers.maxMeleeAttackRange) // move closer
+                //move closer if out of range
+                if (AIVector.Distance(agent.Position, agent.closeEnemyAgents[0].Position) > AIModifiers.maxMeleeAttackRange)
                 {
-                    var enVector = agent.closeEnemyAgents[0].Position - agent.Position;
-                    return new Move(enVector);
+                    var vectorToEnemyAgentPosition = agent.closeEnemyAgents[0].Position - agent.Position;
+                    return new Move(vectorToEnemyAgentPosition);
                 }
                 else //attack
                 {
